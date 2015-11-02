4eeea476da70676a2556130e5295ceccd4634bae
Laura Smith <laura@e3creative.co.uk>
2015-11-02 13:55:41 +0000

    Add background image to holding page

--- wp-content/themes/ls-dev/templates/template-holding.php
+++ wp-content/themes/ls-dev/templates/template-holding.php
@@ -2,8 +2,11 @@
     /**
      * Template Name: Holding Page
      */
+
+$backgroundImage = get_field('background_image');
+
 ?>
-<div class="full-width full-height js-full-height">
+<div class="full-width full-height js-full-height background-img-center" style="background-image:url('<?=$backgroundImage['url']?>')">
     <div class="display-table">
         <div class="display-table-cell text-center">
             <h1><?=get_the_title()?></h1>

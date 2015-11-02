05b30db98b05fba227e61b6dce81dff9b2e0b792
Laura Smith <laura@e3creative.co.uk>
2015-11-02 08:51:51 +0000

    upgrade to ACF pro

--- wp-content/themes/ls-dev/templates/template-holding.php
+++ wp-content/themes/ls-dev/templates/template-holding.php
@@ -6,7 +6,7 @@
 <div class="full-width full-height js-full-height">
     <div class="display-table">
         <div class="display-table-cell text-center">
-            <h1>Testing</h1>
+            <h1><?=get_the_title()?></h1>
         </div>
     </div>
 </div>
